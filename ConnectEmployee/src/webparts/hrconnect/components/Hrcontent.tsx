da69925f9b1fbbe7498364949a4dce4b8817d6fe
TarakaSivakoti <tarak.sivakoti@gmail.com>
2020-02-08 22:17:22 +0530

    removed carousel for Tarak-new

--- ConnectEmployee/src/webparts/hrconnect/components/Hrcontent.tsx
+++ ConnectEmployee/src/webparts/hrconnect/components/Hrcontent.tsx
@@ -85,45 +85,6 @@ export default class Hrcontent extends React.Component<{}, IDatePickerState> {
     const { firstDayOfWeek } = this.state;
     return (
       <div className={style.hrcontent}>
-        <div
-          id="carouselExampleIndicators"
-          className="carousel slide"
-          data-ride="carousel"
-        >
-          <ol className="carousel-indicators">
-            <li
-              data-target="#carouselExampleIndicators"
-              data-slide-to="0"
-              className="active"
-            ></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img
-                src="https://eastus1-mediap.svc.ms/transform/thumbnail?provider=spo&inputFormat=png&cs=fFNQTw&docid=https%3A%2F%2Fcontosohealthsystem.sharepoint.com%3A443%2F_api%2Fv2.0%2Fdrives%2Fb!D3HUnfnOHESMgckvPSxfXTOvaZynM6RLgOVZJ6qKmzCntPKLjvLvTrmUaKsPs1XF%2Fitems%2F015JDSC3TDZ4W3KGI5YNAYOEYABEYG7UXH%3Fversion%3DPublished&access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvY29udG9zb2hlYWx0aHN5c3RlbS5zaGFyZXBvaW50LmNvbUBkMjI2ZDZhOC1lZjM4LTQ3MTQtYTM3MC1lODQyMjNmYWY1M2IiLCJpc3MiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAiLCJuYmYiOiIxNTgxMTUwMTkxIiwiZXhwIjoiMTU4MTE3MTc5MSIsImVuZHBvaW50dXJsIjoibHVCc0JaL0Y0VktKZ3JsdHNSemJaUitiWTZUalVRbzgwVGJKVHgrRThWYz0iLCJlbmRwb2ludHVybExlbmd0aCI6IjEyNiIsImlzbG9vcGJhY2siOiJUcnVlIiwiY2lkIjoiTWpFMk1UTXpPV1l0WlRBMk1DMHdNREF3TFRRMU9UQXRaalF3WVROa056WmxZV0UyIiwidmVyIjoiaGFzaGVkcHJvb2Z0b2tlbiIsInNpdGVpZCI6Ik9XUmtORGN4TUdZdFkyVm1PUzAwTkRGakxUaGpPREV0WXpreVpqTmtNbU0xWmpWayIsIm5hbWVpZCI6IjAjLmZ8bWVtYmVyc2hpcHxzcmluaUBjb250b3NvaGVhbHRoc3lzdGVtLm9ubWljcm9zb2Z0LmNvbSIsIm5paSI6Im1pY3Jvc29mdC5zaGFyZXBvaW50IiwiaXN1c2VyIjoidHJ1ZSIsImNhY2hla2V5IjoiMGguZnxtZW1iZXJzaGlwfDEwMDMyMDAwMzNjNWE0YzlAbGl2ZS5jb20iLCJ0dCI6IjAiLCJ1c2VQZXJzaXN0ZW50Q29va2llIjoiMiJ9.a3gzY0E0dVVhN2pCQWU3Y29KZzhHcjZoakdDNVZ2YWhrUmdaY3g0YmEwcz0&encodeFailures=1&srcWidth=&srcHeight=&width=1000&height=220&action=Access"
-                className="d-block w-50"
-                alt="UC health logo"
-              />
-            </div>
-            <div className="carousel-item">
-              <img
-                src="https://eastus1-mediap.svc.ms/transform/thumbnail?provider=spo&inputFormat=png&cs=fFNQTw&docid=https%3A%2F%2Fcontosohealthsystem.sharepoint.com%3A443%2F_api%2Fv2.0%2Fdrives%2Fb!D3HUnfnOHESMgckvPSxfXTOvaZynM6RLgOVZJ6qKmzCntPKLjvLvTrmUaKsPs1XF%2Fitems%2F015JDSC3TDZ4W3KGI5YNAYOEYABEYG7UXH%3Fversion%3DPublished&access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvY29udG9zb2hlYWx0aHN5c3RlbS5zaGFyZXBvaW50LmNvbUBkMjI2ZDZhOC1lZjM4LTQ3MTQtYTM3MC1lODQyMjNmYWY1M2IiLCJpc3MiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAiLCJuYmYiOiIxNTgxMTUwMTkxIiwiZXhwIjoiMTU4MTE3MTc5MSIsImVuZHBvaW50dXJsIjoibHVCc0JaL0Y0VktKZ3JsdHNSemJaUitiWTZUalVRbzgwVGJKVHgrRThWYz0iLCJlbmRwb2ludHVybExlbmd0aCI6IjEyNiIsImlzbG9vcGJhY2siOiJUcnVlIiwiY2lkIjoiTWpFMk1UTXpPV1l0WlRBMk1DMHdNREF3TFRRMU9UQXRaalF3WVROa056WmxZV0UyIiwidmVyIjoiaGFzaGVkcHJvb2Z0b2tlbiIsInNpdGVpZCI6Ik9XUmtORGN4TUdZdFkyVm1PUzAwTkRGakxUaGpPREV0WXpreVpqTmtNbU0xWmpWayIsIm5hbWVpZCI6IjAjLmZ8bWVtYmVyc2hpcHxzcmluaUBjb250b3NvaGVhbHRoc3lzdGVtLm9ubWljcm9zb2Z0LmNvbSIsIm5paSI6Im1pY3Jvc29mdC5zaGFyZXBvaW50IiwiaXN1c2VyIjoidHJ1ZSIsImNhY2hla2V5IjoiMGguZnxtZW1iZXJzaGlwfDEwMDMyMDAwMzNjNWE0YzlAbGl2ZS5jb20iLCJ0dCI6IjAiLCJ1c2VQZXJzaXN0ZW50Q29va2llIjoiMiJ9.a3gzY0E0dVVhN2pCQWU3Y29KZzhHcjZoakdDNVZ2YWhrUmdaY3g0YmEwcz0&encodeFailures=1&srcWidth=&srcHeight=&width=1000&height=220&action=Access"
-                className="d-block w-50"
-                alt="UC health logo"
-              />
-            </div>
-            <div className="carousel-item">
-              <img
-                src="https://eastus1-mediap.svc.ms/transform/thumbnail?provider=spo&inputFormat=png&cs=fFNQTw&docid=https%3A%2F%2Fcontosohealthsystem.sharepoint.com%3A443%2F_api%2Fv2.0%2Fdrives%2Fb!D3HUnfnOHESMgckvPSxfXTOvaZynM6RLgOVZJ6qKmzCntPKLjvLvTrmUaKsPs1XF%2Fitems%2F015JDSC3TDZ4W3KGI5YNAYOEYABEYG7UXH%3Fversion%3DPublished&access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvY29udG9zb2hlYWx0aHN5c3RlbS5zaGFyZXBvaW50LmNvbUBkMjI2ZDZhOC1lZjM4LTQ3MTQtYTM3MC1lODQyMjNmYWY1M2IiLCJpc3MiOiIwMDAwMDAwMy0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAiLCJuYmYiOiIxNTgxMTUwMTkxIiwiZXhwIjoiMTU4MTE3MTc5MSIsImVuZHBvaW50dXJsIjoibHVCc0JaL0Y0VktKZ3JsdHNSemJaUitiWTZUalVRbzgwVGJKVHgrRThWYz0iLCJlbmRwb2ludHVybExlbmd0aCI6IjEyNiIsImlzbG9vcGJhY2siOiJUcnVlIiwiY2lkIjoiTWpFMk1UTXpPV1l0WlRBMk1DMHdNREF3TFRRMU9UQXRaalF3WVROa056WmxZV0UyIiwidmVyIjoiaGFzaGVkcHJvb2Z0b2tlbiIsInNpdGVpZCI6Ik9XUmtORGN4TUdZdFkyVm1PUzAwTkRGakxUaGpPREV0WXpreVpqTmtNbU0xWmpWayIsIm5hbWVpZCI6IjAjLmZ8bWVtYmVyc2hpcHxzcmluaUBjb250b3NvaGVhbHRoc3lzdGVtLm9ubWljcm9zb2Z0LmNvbSIsIm5paSI6Im1pY3Jvc29mdC5zaGFyZXBvaW50IiwiaXN1c2VyIjoidHJ1ZSIsImNhY2hla2V5IjoiMGguZnxtZW1iZXJzaGlwfDEwMDMyMDAwMzNjNWE0YzlAbGl2ZS5jb20iLCJ0dCI6IjAiLCJ1c2VQZXJzaXN0ZW50Q29va2llIjoiMiJ9.a3gzY0E0dVVhN2pCQWU3Y29KZzhHcjZoakdDNVZ2YWhrUmdaY3g0YmEwcz0&encodeFailures=1&srcWidth=&srcHeight=&width=1000&height=220&action=Access"
-                className="d-block w-50"
-                alt="UC health logo"
-              />
-            </div>
-          </div>
-        </div>
-
         <div className="accordion" id="hrcontent">
           <div className="card">
             <div
@@ -133,7 +94,7 @@ export default class Hrcontent extends React.Component<{}, IDatePickerState> {
               aria-expanded="true"
               aria-controls="collapseOne"
             >
-              <h2 className={cx("mb-0", style.heading, style.pay)}>
+              <h2 className={cx("mb-0", style.heading)}>
                 <button className="btn btn-link" type="button">
                   Pay
                 </button>
@@ -165,14 +126,6 @@ export default class Hrcontent extends React.Component<{}, IDatePickerState> {
               </div>
               <div>
                 <table className="table table-hover">
-                  {/* <thead>
-                    <tr>
-                      <th scope="col">S No</th>
-                      <th scope="col">Pay Stubs</th>
-                      <th scope="col">View</th>
-                      <th scope="col">Download</th>
-                    </tr>
-                  </thead> */}
                   <Tabledata />
                 </table>
               </div>
@@ -186,7 +139,7 @@ export default class Hrcontent extends React.Component<{}, IDatePickerState> {
               aria-expanded="false"
               aria-controls="collapseTwo"
             >
-              <h2 className={cx("mb-0", style.heading, style.pto)}>
+              <h2 className={cx("mb-0", style.heading)}>
                 <button className="btn btn-link collapsed" type="button">
                   PTO
                 </button>
@@ -214,31 +167,6 @@ export default class Hrcontent extends React.Component<{}, IDatePickerState> {
                         <div className="text-center">2</div>
                       </div>
                     </div>
-
-                    {/* <table className={cx("table  table-responsive",style.tableborder)}>
-                      <tr>
-                        <td className={style.tabletd}>
-                        <span className={style.ptoheading}>Prior PTO Balance</span>
-                        </td>
-                        <td className={style.tabletd}>
-                        <span className={style.ptoheading}>Current Year Balance</span>
-                        </td>
-                        <td className={style.tabletd}>
-                        <span className={style.ptoheading}>Accurate Rate</span>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className={style.tabletd}>
-                        <span className="text-center">3</span>
-                        </td>
-                        <td className={style.tabletd}>
-                        <span className="text-center">7</span>
-                        </td>
-                        <td className={style.tabletd}>
-                        <span className="text-center">2</span>
-                        </td>
-                      </tr>
-                    </table> */}
                   </div>
                 </div>
                 <div
@@ -277,7 +205,7 @@ export default class Hrcontent extends React.Component<{}, IDatePickerState> {
               aria-expanded="false"
               aria-controls="collapseThree"
             >
-              <h2 className={cx("mb-0", style.heading, style.benefits)}>
+              <h2 className={cx("mb-0", style.heading)}>
                 <button className="btn btn-link collapsed" type="button">
                   Benefits
                 </button>
@@ -302,7 +230,7 @@ export default class Hrcontent extends React.Component<{}, IDatePickerState> {
               aria-expanded="false"
               aria-controls="collapseFour"
             >
-              <h2 className={cx("mb-0", style.heading, style.dev)}>
+              <h2 className={cx("mb-0", style.heading)}>
                 <button className="btn btn-link collapsed" type="button">
                   Training/Dev
                 </button>
@@ -326,9 +254,9 @@ export default class Hrcontent extends React.Component<{}, IDatePickerState> {
 
   private _showModal = (): void => {
     this.setState({ showModal: true });
-  }
+  };
 
   private _closeModal = (): void => {
     this.setState({ showModal: false });
-  }
+  };
 }
