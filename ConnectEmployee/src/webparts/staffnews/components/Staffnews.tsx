2a9b57854999a7f6ff01504f8f4d91ad053109ad
TarakSivakoti <v-sivsat@microsoft.com>
2020-02-11 11:27:43 +0530

    added news logo webpart

--- ConnectEmployee/src/webparts/staffnews/components/Staffnews.tsx
+++ ConnectEmployee/src/webparts/staffnews/components/Staffnews.tsx
@@ -7,18 +7,10 @@ export default class Staffnews extends React.Component<IStaffnewsProps, {}> {
   public render(): React.ReactElement<IStaffnewsProps> {
     return (
       <div className={ styles.staffnews }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+         <img
+          src="https://m365x520260.sharepoint.com/sites/ConnectEmployee/Shared%20Documents/sys_pos_clr_rgb.png"
+          className={styles.logo}
+        />
       </div>
     );
   }
